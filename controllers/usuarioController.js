7f1455207bae66de61fca6d62779c80c5bf284f1
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2021-05-29 16:03:17 -0300

    creacion de archivo .env y .gitignore

---
+++ controllers/usuarioController.js
@@ -0,0 +1,7 @@
+exports.crearUsuario = (req, res) => {
+    console.log("funcion crear usuario");
+};
+
+exports.obtenerUsuarios = (req, res) => {
+    console.log("funcion obtener usuarios");
+};
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 // Importación de módulos de versiones anteriores
 const express = require('express');
+const mongoose = require("mongoose");
+const usuariosRoutes = require("./routes/usuarios");
 
 // crear el servidor
 const app = express();
+
+// Conectar a mongodb
+mongoose.Promise = global.Promise;
+mongoose.connect(
+    process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}
+)
+
+//importar rutas
+app.use('/api/usuarios', usuariosRoutes);
 
 // puerto y arranque del servidor
 app.listen(4000, () => {
