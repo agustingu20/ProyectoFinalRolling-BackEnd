257b8f0dc0c2e39eb4015e7cc27c1a4b8b8def63
Carolina <karo18delgado@gmail.com>
2021-06-12 21:36:29 -0300

    ruta con productoId

--- routes/productoRoute.js
+++ routes/productoRoute.js
@@ -6,6 +6,7 @@ const authMiddleware = require("../middleware/authMiddleware");
 router.post('/', productoController.createProducto);
 router.get('/:productoId', productoController.getProducto);
 router.get('/', productoController.getProductos);
+
 // router.delete('/:productoId', authMiddleware, productoController.deleteProducto);
 
 
