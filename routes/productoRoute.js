b8dd639f0bf3e3e53021db54978d5757bd342da9
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2021-06-18 19:29:14 -0300

    comentado de authmiddleware en productoRoute

--- routes/productoRoute.js
+++ routes/productoRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const productoController = require('../controllers/productoController');
-const authMiddleware = require("../middleware/authMiddleware");
+// const authMiddleware = require("../middleware/authMiddleware");
 
 router.post('/', productoController.createProducto);
 router.get('/:productoId', productoController.getProducto);
@@ -9,9 +9,4 @@ router.get('/', productoController.getProductos);
 router.delete('/:productoId', productoController.deleteProducto);
 router.put('/', productoController.updateProducto);
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 7050fe9732b3037146a0357a5e0b876e1337039e
 module.exports = router;
