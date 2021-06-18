d6f27f62002329665818c44c4ba6a2661471e2f1
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2021-06-18 18:28:17 -0300

    cambios recibidos del pull

--- routes/productoRoute.js
+++ routes/productoRoute.js
@@ -5,11 +5,16 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 router.post('/', productoController.createProducto);
 router.get('/', productoController.getProductos);
+<<<<<<< HEAD
 router.get('/:productoId', productoController.getProducto);
 
 // router.delete('/:productoId', authMiddleware, productoController.deleteProducto);
 // router.delete('/:productoId', productoController.deleteProducto);
+=======
+router.delete('/:productoId', productoController.deleteProducto);
+>>>>>>> 7050fe9732b3037146a0357a5e0b876e1337039e
 router.put('/', productoController.updateProducto);
+
 
 
 module.exports = router;
